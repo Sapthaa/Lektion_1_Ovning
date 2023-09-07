791024a1e5f4a15287d946640e770559a9d3323d
sapthaa <andre.saptha@hotmail.com>
2023-09-07 19:10:25 +0200

    Test

---
+++ src/Main.java
@@ -0,0 +1,42 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // Uppgift 1
+        // variabel(int) namn(nummer1) = värde(10)
+        int nummer1 = 10;
+        int nummer2 = 15;
+        // plussar nummer1 och 2 tillsammans och skriver ut svaret
+        System.out.println(nummer1+nummer2);
+
+
+        // Uppgift 2
+        // Variabel(int) namn(lista) = { värde }
+        int[] lista = {1, 2, 3};
+        // Skriver ut listans index
+        System.out.println(lista[0]);
+
+        // Uppgift 3
+        // Variabel(String) namn(hej) = värde("Hej)
+        String hej = "Hej";
+        String san = "san";
+        // Skriver ut hej och san tillsammans
+        System.out.println(hej+san);
+
+        // Uppgift 4
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
